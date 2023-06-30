5282a03f7fbb4bdaa184360d941ed0153837d93a
InomjonDev <inomjonismanaliev@gmail.com>
2023-06-30 14:56:55 +0500

    Initial commit

--- src/router/auth/Auth.js
+++ src/router/auth/Auth.js
@@ -3,7 +3,7 @@ import { Outlet, Navigate } from "react-router-dom";
 
 function Auth() {
   const user = localStorage.getItem("user");
-  // const user = true;
+  // const user = false;
   return user ? <Outlet /> : <Navigate to={"/login"} replace />;
 }
 
